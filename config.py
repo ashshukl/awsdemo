170eaad07f06d8a3732b4452374f45d78030be2d
ashshukl <69194233+ashshukl@users.noreply.github.com>
2023-05-17 06:31:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "learningdb.cvb006rbnfyp.us-east-2.rds.amazonaws.com"
 customuser = "learner"
 custompass = "learnerpassword"
-customdb = "learningdb"
+customdb = "learnerdb"
 custombucket = "addempmentor"
 customregion = "us-east-1"
